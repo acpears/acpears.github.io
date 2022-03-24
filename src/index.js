5739208747982d3e9524e7cc7669299a059e3bf9
acpears <33049051+acpears@users.noreply.github.com>
2022-03-24 18:28:17 -0400

    updated router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 // import 'foundation-sites/dist/css/foundation.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -15,7 +15,7 @@ import App from './App.js'
 
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>
+    </HashRouter>
     , document.getElementById('app'));
